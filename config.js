ddf2cf1d314daa007c473ee5f3286e4b7339874a
Jakub Wiśniewski <mrkubasny@gmail.com>
2019-04-03 13:53:40 +0200

    in porgress

---
+++ config.js
@@ -0,0 +1,5 @@
+module.export = {
+    'GOOGLE_CLIENT_ID': '862628574442-rs3n5eu1i3t83bp4lb1qru2g0g5rqs6e.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'GuBbhyyoUqPnJ_G6M8330XFp',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
